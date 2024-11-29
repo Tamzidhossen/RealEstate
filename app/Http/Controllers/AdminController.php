1149b9fe4404a418a2420a8f1e61b629e88f0eb8
Tamzid Hossen <tamzidhossen366@gmail.com>
2024-11-29 22:31:20 +0600

    New RealeState File Added

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -30,7 +30,39 @@ class AdminController extends Controller
 
     public function AdminProfile(){
         $id = Auth::user()->id;
-        $adminData = User::find($id);
-        return view('admin.admin_profile_view', compact('adminData'));
+        $profileData = User::find($id);
+        return view('admin.admin_profile_view', compact('profileData'));
+    }
+
+    public function AdminProfileStore(Request $request){
+        $profileData = User::find(Auth::user()->id);
+        
+        if($request->photo != ''){
+            if($profileData->photo != ''){
+                $delete_form = public_path('uploads/admin_images/'. $profileData->photo);
+                unlink($delete_form);
+            }
+
+            $file = $request->photo;
+            $extension = $file->extension();
+            $file_name = uniqid().'.'. $extension;
+            // return $file_name;
+
+            $file->move(public_path('uploads/admin_images'), $file_name);
+        }
+        User::find(Auth::user()->id)->update([
+            'username' => $request->username,
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'address'=> $request->address,
+            'photo' => $file_name,
+        ]);
+
+        $notification = array(
+            'message' => "Admin Profile Updated Successfully",
+            'alert-type' => 'success'
+        );
+        return back()->with($notification);
     }
 }
